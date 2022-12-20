f3eb63837cb0f567b76a92621b44cc5d3f568b7a
Amine <ka_adjou@esi.dz>
2022-12-20 21:55:23 +0100

    Project Setup/Project Structure

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="bg-black text-white font-Inter text-9xl flex items-center justify-center w-full h-screen ">
+      Hello TP IGL!!
+    </div>
+  );
+}
