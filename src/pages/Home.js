28e7df7a1d4df4e25437f6de58cd6f34517b857e
Amine <ka_adjou@esi.dz>
2022-12-23 20:01:33 +0100

    Home page Mobile version

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 import HomePic from "../assets/homepage-picture.svg";
 export default function Home() {
   return (
-    <div className="bg-akkar-white-creme w-full ">
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col pl-28 pt-20 gap-16 ">
-          <div className="font-medium font-Inter text-akkar-black text-5xl w-[500px] leading-tight ">
+    <div className="bg-akkar-white-creme w-full h-screen pb-14 ">
+      <div className="flex md:flex-row flex-col-reverse items-center justify-between">
+        <div className="flex flex-col items-center md:items-start md:pl-28 pt-20 md:gap-16 gap-5 ">
+          <div className="font-medium font-Inter text-akkar-black text-3xl text-center md:text-left md:text-5xl w-[500px] leading-tight ">
             Find your place of dream with AKKAR
           </div>
-          <div className=" text-3xl font-Inter text-akkar-gray font-normal w-[480px]">
+          <div className=" text-xl text-center md:text-left md:text-3xl font-Inter text-akkar-gray font-normal w-[480px]">
             Buying real estate is not only the best way, the quickest way, the
             safest way, but the only way to become wealthy
           </div>
           <Link to="/login">
             <button
-              className=" flex items-center gap-3 font-medium bg-akkar-orange text-white text-2xl font-Inter py-6 px-20 rounded-[3px] hover:bg-akkar-orange-second hover:text-black
+              className=" flex items-center gap-3 font-medium bg-akkar-orange text-white text-xl md:text-2xl font-Inter py-2 md:py-6 px-14 rounded-[3px] hover:bg-akkar-orange-second hover:text-black
     duration-200"
             >
               <ion-icon size="large" name="logo-google"></ion-icon> Login with
@@ -23,7 +23,7 @@ export default function Home() {
             </button>
           </Link>
         </div>
-        <img className=" h-[650px]" src={HomePic} />
+        <img className=" md:h-[650px]" src={HomePic} />
       </div>
     </div>
   );
