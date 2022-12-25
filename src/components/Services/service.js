7cfa6e67c880fe3a210712d9fd1d322a3219e8ad
Yanis02 <ka_djouima@esi.dz>
2022-12-25 01:40:19 +0100

    added our services section in home page

---
+++ src/components/Services/service.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Services(props) {
+    return(
+        <div className="  p-5 w-[270px] h-[350px] border-2 border-akkar-orange md:p-8 bg-white md:w-[400px] md:h-[500px] rounded-[8px]">
+        <img src={props.img}></img>
+        <h3 className="font-Inter text-akkar-orange text-base  md:text-xl md:pt-8 pt-4 text-center  font-akkar-bold">{props.title}</h3>
+        <p className="font-Inter text-akkar-orange-third text-[12px] font-light md:text-lg text-center">{props.description}</p>
+        </div>
+          );
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,17 @@ module.exports = {
         "akkar-white-creme": "#FBFBFA",
         "akkar-gray": "#50463C",
         "akkar-black": "#200E01",
+        "akkar-orange-third" : "#B28666"
       },
       fontFamily: {
         Inter: ["Inter", "sans-serif"],
       },
+      fontWeight:{
+        "akkar-bold" :500,
+      },
+      boxShadow:{
+        "akkar-shadow": "0px -10px 17px 14px #E95913"
+      }
     },
   },
   plugins: [],
