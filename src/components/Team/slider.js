c03e4bb9336c7058271c8ce52c16e0c457ec8574
Yanis02 <ka_djouima@esi.dz>
2022-12-27 22:35:39 +0100

    fixing responsivity

--- src/components/Team/slider.js
+++ src/components/Team/slider.js
@@ -14,7 +14,7 @@ export default function Slider() {
   return (
 <div >
 
-<Swiper navigation={true} modules={[Navigation]} className=" w-[500px] h-[400px]  sm:w-[700px] sm:h-[500px] md:w-[1300px] md:h-[550px] " slidesPerView={isMobile ? 1 : 2}>
+<Swiper navigation={true} modules={[Navigation]} className=" w-[500px] h-[400px]  sm:w-[700px] sm:h-[500px] md:w-[1300px] md:h-[550px] mt-10 sm:mt-0 " slidesPerView={isMobile ? 1 : 2}>
         <SwiperSlide className="flex flex-row justify-evenly items-center"><Card name="Amine ADJOU" description="1CS G08 Student Front-end web developer Jango expert & machine learning passionate" ></Card>    </SwiperSlide>
         <SwiperSlide className="flex flex-row justify-evenly items-center"><Card name="Yanis DJOUIMA" description="1CS G08 Student Front-end web developer Jango expert & machine learning passionate"></Card>    </SwiperSlide>
         <SwiperSlide className="flex flex-row justify-evenly items-center"><Card name="Fouad KHELIL" description="1CS G08 Student Front-end web developer Jango expert & machine learning passionate"></Card>    </SwiperSlide>
