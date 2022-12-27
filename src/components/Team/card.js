cc628aec1b646b0f6b923bc103f4a8bcb7e5380f
Yanis02 <ka_djouima@esi.dz>
2022-12-27 21:32:42 +0100

    added arc to the team members pictures

--- src/components/Team/card.js
+++ src/components/Team/card.js
@@ -8,9 +8,12 @@ import github from "../../assets/github.svg"
 export default function card(props) {  //ki ndiro l contenu w YB3TOLI TSAWRHOM W NSGMO DESCRIPTION  npassi data bl props
     return (
         <div className=" w-[160px] md:w-[340px] h-[260px] md:h-[500px] bg-white rounded-[3px] flex flex-col  items-center border-2 border-akkar-brown-second">
-        <div className=" h-[50px] w-[50px] md:h-[140px] md:w-[140px] rounded-[50%] md:mt-[60px] mt-[30px] ">
+       <div className="h-[60px] w-[60px] md:h-[160px] md:w-[160px] rounded-[50%] md:mt-[60px] mt-[30px] bg-white flex flex-row justify-center items-center border-[2px] md:border-[4px] border-l-akkar-orange border-b-akkar-orange border-r-white border-t-white">
+        <div className=" h-[50px] w-[50px] md:h-[140px] md:w-[140px] rounded-[50%]">
             <img src={amine} className="w-[100%] h-[100%] rounded-[50%] object-cover"></img>
         </div>
+       </div>
+        
         <p className="md:text-2xl text-akkar-orange md:mt-[35px] font-medium text-center text-lg ">Amine Adjou</p>
         <p className=" text-sm text-center text-akkar-brown-second mt-[2px] md:mt-[20px] md:text-xl">1CS G08 Student Front-end web developer Jango expert & machine learning passionate</p>
           <div className=" flex flex-row  w-full md:h-[60px] justify-evenly items-center mt-[15px] md:mt-[40px]">
