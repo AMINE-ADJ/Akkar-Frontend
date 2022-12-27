065a21fff202493fa059beb02877efed48346dd3
Amine <ka_adjou@esi.dz>
2022-12-27 21:53:39 +0100

    Fixing Swiper

--- src/components/Team/card.js
+++ src/components/Team/card.js
@@ -11,8 +11,8 @@ export default function card(props) {  //ki ndiro l contenu w YB3TOLI TSAWRHOM W
         <div className=" h-[50px] w-[50px] md:h-[140px] md:w-[140px] rounded-[50%] md:mt-[60px] mt-[30px] ">
             <img src={amine} className="w-[100%] h-[100%] rounded-[50%] object-cover"></img>
         </div>
-        <p className="md:text-2xl text-akkar-orange md:mt-[35px] font-medium text-center text-lg ">Amine Adjou</p>
-        <p className=" text-sm text-center text-akkar-brown-second mt-[2px] md:mt-[20px] md:text-xl">1CS G08 Student Front-end web developer Jango expert & machine learning passionate</p>
+        <p className="md:text-2xl text-akkar-orange md:mt-[35px] font-medium text-center text-lg ">{props.name}</p>
+        <p className=" text-sm text-center text-akkar-brown-second mt-[2px] md:mt-[20px] md:text-xl">{props.description}</p>
           <div className=" flex flex-row  w-full md:h-[60px] justify-evenly items-center mt-[15px] md:mt-[40px]">
             <a href="facebook.com"><div className=" w-[25px] h-[25px] md:w-[35px] md:h-[35px]"><img src={facebook} className="w-[100%] h-[100%] object-cover"></img></div></a>
             <a href="linkedin.com"><div className=" w-[25px] h-[25px] md:w-[35px] md:h-[35px]"><img src={linkedin} className="w-[100%] h-[100%] object-cover"></img></div></a>
