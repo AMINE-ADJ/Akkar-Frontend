1ea8e03b4a1c2b56857f4dda088f8a8e5ce7c13f
Yanis02 <ka_djouima@esi.dz>
2023-01-09 18:19:10 +0100

    added post's details page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomeBeforeAuth from "./pages/HomeBeforeAuth";
 import HomeAfterAuth from "./pages/HomeAfterAuth";
+import Details from "./pages/Details.js";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomeBeforeAuth />} />
         <Route path="/authenticated" element={<HomeAfterAuth />} />
+        <Route path="/details" element={<Details />} />
+
       </Routes>
     </div>
   );
