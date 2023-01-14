3c52d800fbc54b6e743b410fb3f8197d146f9095
Yanis02 <ka_djouima@esi.dz>
2023-01-14 16:15:42 +0100

    adding validation to searchPage

--- src/components/HomeAfterAuth/Post.js
+++ src/components/HomeAfterAuth/Post.js
@@ -7,37 +7,37 @@ import { Link } from "react-router-dom";
 export default function Post(props) {
     return(
         <Link to={`/authenticated/detailes/${props.Postid}`}>
-        <div  className=" m-10 flex flex-col justify-between items-center w-[420px] h-[510px] rounded-[7px] border-2 border-[#E7E9EB] cursor-pointer">
-                <div className="w-[420px] h-[270px] rounded-[7px] ">
+        <div  className=" m-10 flex flex-col justify-between items-center w-[240px] h-[370px] rounded-[7px] border-2 border-[#E7E9EB] cursor-pointer">
+                <div className="w-[240px] h-[170px] rounded-[7px] ">
                     <img src={props.img} className="w-[100%] h-[100%] object-cover rounded-t-[7px] mt-[-2px]"></img>
                 </div>
 
-                     <p className="text-left text-black font-medium text-[25px] m-[20px] ">{props.title}</p>
-                    <div className="flex flex-row justify-between p-5 items-center w-[420px] h-[50px] mt-[-10px]">
-                              <div className="flex flex-row justify-center items-center gap-[10px] ">
-                                              <img src={localisation} className="w-[32px] h-[32px]"></img>
-                                              <p className="text-lg text-[#6D737A]"> {props.localisation}</p>
+                     <p className="text-left text-black font-medium text-[25px] m-[10px] text-[20px] ">{props.title}</p>
+                    <div className="flex flex-row justify-around   items-center w-[240px] h-[50px] mt-[-10px]">
+                              <div className=" flex flex-row justify-center items-center gap-[2px] ">
+                                              <img src={localisation} className="w-[19px] h-[19px]"></img>
+                                              <p className="text-[18px] text-[#6D737A]"> {props.localisation}</p>
                               </div>
 
-                              <div className="flex flex-row justify-center items-center gap-[10px] ">
-                                              <img src={arrows} className="w-[32px] h-[32px]"></img>
-                                              <p className="text-lg text-[#6D737A]"> {props.area}</p>
+                              <div className="flex flex-row justify-center items-center gap-[2px] ">
+                                              <img src={arrows} className="w-[19px] h-[19px]"></img>
+                                              <p className="text-[18px] text-[#6D737A]"> {props.area}</p>
                               </div>
 
-                              <div className="flex flex-row justify-center items-center gap-[10px]">
-                                              <img src={clock} className="w-[32px] h-[32px]"></img>
-                                              <p className="text-lg text-[#6D737A]"> {props.time}</p>
+                              <div className="flex flex-row justify-center items-center gap-[0px]">
+                                              <img src={clock} className="w-[19px] h-[19px]"></img>
+                                              <p className="text-sm text-[#6D737A]"> {props.time}</p>
                               </div>
                     </div>
                   
-                            <div className="w-[400px] h-[2px] bg-[#E7E9EB] mb-[-20px]" >
+                            <div className="w-[240px] h-[2px] bg-[#E7E9EB] mb-[-20px]" >
 
                             </div>
                
-                    <div className="flex flex-row justify-between p-5 items-center w-[420px] md:mt-[10px]">
-                        <p className="text-xl text-akkar-orange font-medium ">{props.price}</p>
+                    <div className="flex flex-row justify-between p-5 items-center w-[240px] md:mt-[10px]">
+                        <p className="text-lg text-akkar-orange font-medium ">{props.price}</p>
                         <button>
-                            <div className="flex flex-row justify-center bg-akkar-orange-second w-[40px] h-[40px]">
+                            <div className="flex flex-row justify-center bg-akkar-orange-second w-[30px] h-[30px]">
                                   <img src={send}></img>
                                </div>
                         </button>
